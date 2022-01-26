b4e1e8f66b4f666bf10611dbc4abf09d504868c4
Alaory <50534770+Alaory@users.noreply.github.com>
2022-01-26 15:04:49 +0200

    Update tic_tac_to.cpp

--- tic_tac_to.cpp
+++ tic_tac_to.cpp
@@ -1,199 +1,109 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int right(int (&boardarr)[10][10],int x,int y){
-    int d=0;
-    if(y == 9 || x == 9 || y == 0 || x == 0){
-        return d=7;
-    }
-    if(boardarr[x][y+1] == 2 )
-    {
-        d++;
-        if(boardarr[x+1][y+1] == 2){
-            d++;
-            if( boardarr[x+1][y] == 2){
-                d++;
-                if(boardarr[x+1][y-1] == 2){
-                    d++;
-                    if(boardarr[x][y-1] == 2){
-                        d++;
+// int Dist(int x1,int y1,int x2,int y2){
+//     return (sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)));
+// }
+
+void slecetmove(int x,int y, int (&board)[10][10], int &fav,int &numofrec,int& round){
+        if(numofrec <= 0)
+            return;
+
+        int movy=-1,movx=-1,ar=3;
+        if(y == 0)
+            movy =0;
+        if(x == 0)
+            movx =0;
+
+        
+        if((x == 4 && y == 4 ) || (x == 5 && y == 5) || (x == 4 && y == 5) || (x == 5 && y == 4) ){
+            fav += 80*10;
+        }
+
+        
+        for(int i=y + movy;i<y+ar && (i <= 9 || i >= 0);i++)
+        {
+            for(int j=x + movx;j<x+ar && (j <= 9 || j>= 0);j++)
+            {
+                if(board[j][i] == 1)
+                {
+                    fav++;
+                    numofrec--;
+                    slecetmove(j,i,board,fav,numofrec,round);
+                    int currentrow = board[j + ((j-x)*2)][i+ ((i-y)*2)];
+                    if(currentrow == 1){ //row check
+                        fav+=2;
+                        if(currentrow == 1){ //row check
+                            fav+=2;
+                        }else if(currentrow == 0){ // check for empty rows
+                            fav+=2;
+                        }else{
+                            fav --;
+                        }
                     }
+                    
                 }
             }
         }
-    }
-    if(boardarr[x][y+1] == 1 )
-    {
-        d--;
-        if(boardarr[x+1][y+1] == 1){
-            d--;
-            if( boardarr[x+1][y] == 1){
-                d--;
-                if(boardarr[x+1][y-1] == 1){
-                    d--;
-                    if(boardarr[x][y-1] == 1){
-                        d--;
-                    }
-                }
-            }
-        }
-    }
-    return d;
 }
 
-int up(int (&boardarr)[10][10],int x,int y){
-    int d=0;
-    if(y == 9 || x == 9 || y == 0 || x == 0){
-        return d=7;
-    }
-    if(boardarr[x][y+1] == 2 )
-    {
-        d++;
-        if(boardarr[x+1][y+1] == 2){
-            d++;
-            if( boardarr[x+1][y] == 2){
-                d++;
-                if(boardarr[x-1][y] == 2){
-                    d++;
-                    if(boardarr[x-1][y+1] == 2){
-                        d++;
-                    }
-                }
-            }
-        }
-    }
-    if(boardarr[x][y+1] == 1 )
-    {
-        d--;
-        if(boardarr[x+1][y+1] == 1){
-            d--;
-            if( boardarr[x+1][y] == 1){
-                d--;
-                if(boardarr[x-1][y] == 1){
-                    d--;
-                    if(boardarr[x-1][y+1] == 1){
-                        d--;
-                    }
-                }
-            }
-        }
-    }
-    return d;
-}
-int left(int (&boardarr)[10][10],int x,int y){
-    int d=0;
-    if(y == 9 || x == 9 || y == 0 || x == 0){
-        return d=7;
-    }
-    if(boardarr[x][y+1] == 2 )
-    {
-        d++;
-        if(boardarr[x-1][y+1] == 2){
-            d++;
-            if( boardarr[x-1][y] == 2){
-                d++;
-                if(boardarr[x-1][y-1] == 2){
-                    d++;
-                    if(boardarr[x][y-1] == 2){
-                        d++;
-                    }
-                }
-            }
-        }
-    }
-    if(boardarr[x][y+1] == 1 )
-    {
-        d--;
-        if(boardarr[x-1][y+1] == 1){
-            d--;
-            if( boardarr[x-1][y] == 1){
-                d--;
-                if(boardarr[x-1][y-1] == 1){
-                    d--;
-                    if(boardarr[x][y-1] == 1){
-                        d--;
-                    }
-                }
-            }
-        }
-    }
-    return d;
-}
-int down(int (&boardarr)[10][10],int x,int y){
-   int d=0;
-   if(y == 9 || x == 9 || y == 0 || x == 0){
-        return d=7;
-    }
-    if(boardarr[x][y-1] == 2 )
-    {
-        d++;
-        if(boardarr[x+1][y-1] == 2){
-            d++;
-            if( boardarr[x+1][y] == 2){
-                d++;
-                if(boardarr[x-1][y] == 2){
-                    d++;
-                    if(boardarr[x-1][y-1] == 2){
-                        d++;
-                    }
-                }
-            }
-        }
-    }
-    if(boardarr[x][y-1] == 1 )
-    {
-        d--;
-        if(boardarr[x+1][y-1] == 1){
-            d--;
-            if( boardarr[x+1][y] == 1){
-                d--;
-                if(boardarr[x-1][y] == 1){
-                    d--;
-                    if(boardarr[x-1][y-1] == 1){
-                        d--;
-                    }
-                }
-            }
-        }
-    }
-    return d;
-}
 
 int main()
 {
 
-    int board[10][10],selectedmove;
+    int board[10][10],selectedmove=0,round=0,valid=0;// if 0 its empty if 1 then its yours if 2 the enemy has it
     bool first = true;
-    // if 0 its empty if 1 then its yours if 2 the enemy has it
-    for(int i =0;i<10;i++){
-        for(int g=0;g<0;g++){
-            board[i][g] = 0;
-        }
-    }
-
-
+    
     while (1) {
+
+        if(round ==0 || round ==100){
+            for(int i =0;i<10;i++){
+                for(int g=0;g<0;g++){
+                board[i][g] = 0;
+                }
+            }
+            cerr << "cleaning board" << endl;
+        }
+
+        
+
         int opponent_row;
-        int danger=10; // The coordinates of your opponent's last move
+        int danger=0; // The coordinates of your opponent's last move
         int opponent_col;
         cin >> opponent_row >> opponent_col; cin.ignore();
         int valid_action_count; // the number of possible actions for your next move
         cin >> valid_action_count; cin.ignore();
-        board[opponent_row][opponent_col]= 2;
-        int avi[valid_action_count][2];
-        for (int i = 0; i < valid_action_count; i++) {
-            cin >> avi[i][0] >> avi[i][1]; cin.ignore();
-            //cerr << "row: " << row << " col: " << col << endl;
+
+        if(valid_action_count > valid){
+            valid = valid_action_count;
         }
 
+        board[opponent_row][opponent_col]= 2;
+        int avi[valid_action_count][2];
+        
+        
+        for (int i = 0; i < valid_action_count; i++) {
+            cin >> avi[i][0] >> avi[i][1]; cin.ignore();
+        }
+        
+        
+
         for(int i=0;i<valid_action_count;i++){
-            int someof = up(board,avi[i][0],avi[i][1]) + down(board,avi[i][0],avi[i][1]) +left(board,avi[i][0],avi[i][1]) +right(board,avi[i][0],avi[i][1]);
-            if(someof/4 < danger){
-                danger = someof/4;
+            int fav = 1;
+            int numofrecerkon = 3;
+            slecetmove(avi[i][0],avi[i][1],board,fav,numofrecerkon,round);
+            if(fav > danger){
+                danger = fav;
                 selectedmove = i;
             }
         }
+
+
         cout << avi[selectedmove][0] << " " << avi[selectedmove][1] << endl;
         board[avi[selectedmove][0]][avi[selectedmove][1]] = 1;
+        round+=2;
     }
 }
+
+
